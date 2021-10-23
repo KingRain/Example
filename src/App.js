960c346ee46650896b34ce2f97188f3ff9c7e264
RainBoi <samjoe55555@gmail.com>
2021-10-23 05:11:04 +0000

    oki

--- src/App.js
+++ src/App.js
@@ -1,15 +1,76 @@
 import { GlobalStyle } from "./globalStyles";
 import { lazy, Suspense } from "react";
 import lottie from 'lottie-web';
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
 
 const Home = lazy(() => import("./Pages/Home"));
 const Header = lazy(() => import("./components/Header/index"));
 const Footer = lazy(() => import("./components/Footer/index"));
 const ScrollToTop = lazy(() => import("./components/ScrollToTop/index"));
 
+function Product({ product }) {
+  const [paidFor, setPaidFor] = useState(false);
+  const [error, setError] = useState(null);
+  const paypalRef = useRef();
+
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: product.description,
+                amount: {
+                  currency_code: 'USD',
+                  value: product.price,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          setPaidFor(true);
+          console.log(order);
+        },
+        onError: err => {
+          setError(err);
+          console.error(err);
+        },
+      })
+      .render(paypalRef.current);
+  }, [product.description, product.price]);
+
+  if (paidFor) {
+    return (
+      <div>
+        <h1>Congrats, you just bought {product.name}!</h1>
+        <img alt={product.description} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {error && <div>Uh oh, an error occurred! {error.message}</div>}
+      <h1>
+        {product.description} for ${product.price}
+      </h1>
+      <img alt={product.description} src={product.image} width="200" />
+      <div ref={paypalRef} />
+    </div>
+  );
+}
+
 function App() {
   const container = useRef(null)
+  const product = {
+    price: 777.77,
+    name: 'example',
+    description: 'Lorem Isopum',
+  };
 
   useEffect(() => {
     lottie.loadAnimation({
@@ -29,6 +90,9 @@ function App() {
         <ScrollToTop />
         <Header />
         <Home />
+        <div className="App">
+          <Product product={product} />
+        </div>
         <Footer />
       </Suspense>
     </>

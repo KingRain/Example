b1a88ad289d445b89aedfdd50c9777899b7cb8f7
RainBoi <samjoe55555@gmail.com>
2021-12-23 08:45:12 +0000

    yes

--- src/Sections/Contact/index.js
+++ src/Sections/Contact/index.js
@@ -7,7 +7,7 @@ import styled from "styled-components";
 const ContactSection = styled.section`
   width: 100vw;
   padding: calc(2.5rem + 2.5vw) 0;
-  background-color: white;
+  background-color: black;
   display: flex;
   flex-direction: column;
   /* justify-content: center; */
